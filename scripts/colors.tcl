c41b065dd440589a60496136c46be54799914fa4
Laurent Laborde <kerdezixe@gmail.com>
2013-01-17 15:07:15 +0100

    initial commit

---
+++ scripts/colors.tcl
@@ -0,0 +1,35 @@
+namespace eval colors {
+
+###	Old colors
+#	variable canvas_bg "#e0e0ff"
+#	variable if_bg "#ffffb0"
+#	variable for_bg "#c0ffc0"
+#	variable case_bg "#ffe0d0"
+#	variable action_bg "#ffffff"	
+#	variable text_fg "#000000"
+#	variable line_fg "#000000"
+#	variable vertex_fg "#000000"
+#	variable comment_bg "#e0e0e0"
+	
+
+	variable canvas_bg "#d0d0ff"
+	variable if_bg "#ffffc0"
+	variable for_bg "#cfffcf"
+	variable case_bg "#ffefdf"
+	variable action_bg "#ffffff"	
+	variable text_fg "#000000"
+	variable line_fg "#000000"
+	variable vertex_fg "#000000"
+	variable comment_bg "#e0e0e0"
+
+### Dark night
+#	variable canvas_bg "#001027"
+#	variable if_bg "#171700"
+#	variable for_bg "#001700"
+#	variable case_bg "#170000"
+#	variable action_bg "#000000"	
+#	variable text_fg "#ffffb0"
+#	variable line_fg "#ccccdc"
+#	variable vertex_fg "#000040"
+#	variable comment_bg "#303030"
+}
---
+++ unittest/regenerate.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+../drakon_gen.tcl -in ../generators/c.drn
+../drakon_gen.tcl -in ../generators/cpp.drn
+../drakon_gen.tcl -in ../generators/cs.drn
+
+../drakon_gen.tcl -in ../generators/cycle_body.drn
+../drakon_gen.tcl -in ../generators/java.drn
+../drakon_gen.tcl -in ../generators/lua.drn
+
+
+../drakon_gen.tcl -in ../generators/node_sorter.drn
+../drakon_gen.tcl -in ../generators/nogoto.drn
+../drakon_gen.tcl -in ../generators/python.drn
+
+
+../drakon_gen.tcl -in ../scripts/alt_edit.drn
+../drakon_gen.tcl -in nogoto_src.drn
+../drakon_gen.tcl -in erltest.drn
